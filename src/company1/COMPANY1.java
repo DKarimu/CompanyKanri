9cbc0aa9736a0a3a1626b9d31867689424a148f2
DKarimu <80462313+DKarimu@users.noreply.github.com>
2021-06-30 17:58:07 +0900

    Update COMPANY1.java

--- src/company1/COMPANY1.java
+++ src/company1/COMPANY1.java
@@ -1,11 +1,5 @@
 package company1;
 
-import Entity.Employee_Phones;
-import Forms.FRMDepartment;
-import Forms.FRMEmployee;
-import Forms.FRMProject;
-import Forms.FRMWorkon;
-import Forms.frmTest;
 import Forms.frmlogen;
 
 public class COMPANY1 {
