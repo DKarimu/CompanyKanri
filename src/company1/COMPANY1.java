4daf591b443fb1e6feea2fe2eb860be462259924
DKarimu <80462313+DKarimu@users.noreply.github.com>
2021-06-30 17:57:06 +0900

    Update COMPANY1.java

--- src/company1/COMPANY1.java
+++ src/company1/COMPANY1.java
@@ -11,13 +11,8 @@ import Forms.frmlogen;
 public class COMPANY1 {
 
     public static void main(String[] args) {
-        //Tools.openForm(new frmTest());
+
         Tools.openForm(new frmlogen());
-        //Tools.openForm(new FRMEmployee());
-        //Tools.openForm(new FRMDepartment());
-        //Tools.openForm();
-        //Tools.openForm(new FRMProject());
-        //Tools.openForm(new FRMWorkon());
     }
 
 }
